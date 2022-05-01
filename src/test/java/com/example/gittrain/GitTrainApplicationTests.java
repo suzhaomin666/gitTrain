f0e9485ed6820bbc3a3a8e5cefdc0d159873b42d
suzhaomin <yfszm666@outlook.com>
2022-05-01 20:10:31 +0800

    Initial commit

---
+++ src/test/java/com/example/gittrain/GitTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
